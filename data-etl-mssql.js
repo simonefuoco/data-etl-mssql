b2990bb081caad9f24190c0840b06546a3689c24
simonefuoco <simofstudies@gmail.com>
2018-10-29 10:46:44 +0100

    1.0.5

--- data-etl-mssql.js
+++ data-etl-mssql.js
@@ -1,122 +1,72 @@
-/**
- * Microsoft SQL-Server Extractor Module.
- * @author Simone Fuoco <simofstudies@gmail.com>
- * @module module:data-etl-mssql
- * @license MIT
- * @see {@link https://github.com/simonefuoco/data-etl | data-etl}
- * 
- * @requires tedious
- * @see {@link http://tediousjs.github.io/tedious/ | tedious Documentation}
- * @see {@link https://github.com/tediousjs/tedious | tedious Repository}
- * @see {@link https://david-dm.org/tediousjs/tedious | tedious Dependencies} {@link david-dm.org | david-dm.org}}
- * @see {@link https://www.npmjs.com/package/tedious | tedious NPM}
- * @see {@link https://travis-ci.org/tediousjs/tedious | tedious Travis CI Build}
- * @see {@link https://ci.appveyor.com/project/tediousjs/tedious | tedious AppVeyor Build}
- * @see {@link https://tediousjs-slack.herokuapp.com/ | tedious Slack}
- */
+const {Connection, Request} = require('tedious');
 
-const tedious = require('tedious');
-const { Readable } = require('stream');
-const queue = [];
-const isFinished = false;
+//TODO - too many promises
 
-/**
- * @typedef {Object} TediousConnectionConfig
- * @see {@link http://tediousjs.github.io/tedious/api-connection.html | tedious connection documentation}
- */
+class Extractor
+{
+    constructor(args) {
+        this.ready = false;
+        this.conn = null;
+        this.promises = [];
+        this.req = null;
 
-/**
- * @typedef {Object|Array} EventRowColumns
- * @see {@link http://tediousjs.github.io/tedious/api-request.html#event_row| Event Row Columns Documentation}
- */
+        this.config = args.config;
+        this.query = args.query;
+        this.cache = args.cache;
 
-/**
- * @typedef ExtractorArgs
- * @property {TediousConnectionConfig} config - Tedious configuration object.
- * @property {string} query - SQL Query.
- */
+        this.generator = function * () {
 
-/**
- * @function mssqlImport
- * @private
- * @param {ExtractorArgs} args - Extractor arguments.
- */
-const mssqlImport = (args, state) => {
-    state.conn = new tedious.Connection(args.config);
-    state.conn.on('connect', afterConnect(args, state));
-};
+            const next = async () => {
+                this.cache.decrementCounter;
+                let obj = await this.cache.readOne();
+                await this.cache.delete(obj);
+                return obj;
+            };
 
-/**
- * @function afterConnect
- * @private
- * @param {ExtractorArgs} args - Extractor arguments.
- */
-const afterConnect = (args, state) => {
-    return (err) => {
-        if(err) throw Error("error tedious connection");
-        state.req = new tedious.Request(args.query, afterSqlStream(args, state));
-        state.req.on('row', forEachRow(args, state));
-        state.conn.execSql(state.req);
-    };
-};
-
-/**
- * @function forEachRow
- * @private
- * @param {ExtractorArgs} args - Extractor arguments.
- */
-const forEachRow = (args, state) => {
-    return (columns) => {
-        save(columns, args, state);
-    };
-};
-
-/**
- * @function save
- * @private
- * @param {EventRowColumns} columns
- * @param {ExtractorArgs} args - Extractor arguments.
- */
-const save = (columns, args, state) => {
-    let doc = {};
-    columns.forEach(col => {
-        doc[col.metadata.colName] = col.value;
-    });
-    //args.store.insert(doc);
-    //state.stream.push(doc);
-    queue.push(doc);
-};
-
-/**
- * @function afterSqlStream
- * @private
- * @param {ExtractorArgs} args - Extractor arguments.
- */
-const afterSqlStream = (args, state) => {
-    return (err, rowCount) => {
-        if(err && !rowCount) throw new Error("error mmsql query execution");
-        //await Promise.all(state.promises);
-        //state.stream.push(null);
-        isFinished = true;
-    };
-}
-
-/**
- * Extract data from [Microsoft SQL-Server]{@link https://www.microsoft.com/sql-server}.
- * @function extract
- * @public
- * @param {Object} args - extractor arguments.
- * @return 
- */
-module.exports.extract = (args) => {
-    let state = {};
-    mssqlImport(args, state);
-    return new Readable({
-        objectMode: true,
-        read() {
-            if (queue.length) {
-                this.push(queue.pop());
+            while (!this.cache.isEmpty()) {
+                yield next();
             }
+
+        }.bind(this)();
+    }
+
+    isReady() {
+        return this.ready;
+    }
+
+    init() {
+        this.conn = new Connection(this.config)
+        this.conn.on('connect', (err) => {
+            if(err) throw Error("error tedious connection");
+            this.req = new Request(this.query, (err, rowCount) => {
+                if(err && !rowCount) throw new Error("error mmsql query execution");
+                Promise.all(state.promises)
+                .then(values => {
+                    ready = true;
+                })
+                .catch(reason => {
+                    throw new Error("mssql insert cache failed");
+                });
+            });
+            this.req.on('row', (columns) => {
+                let doc = {};
+                for (const col of columns) {
+                    doc[col.metadata.colName] = col.value;
+                }
+                this.promises.push(this.cache.create(doc));
+            });
+            this.conn.execSql(this.req);
+        });
+    }
+
+    extract() {
+        let {value, done} = this.generator.next().value;
+        if (done === false) {
+            return false;
+        } else {
+            return value;
         }
-    });
+    }
 }
+
+
